177e409787e19fba8de3f17b3331613421b292e0
Mashu Ajmera <mashuajmera@gmail.com>
2021-02-26 12:12:13 +0530

    coin change using DP

---
+++ coin_change.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4, w = 15, i, j;
+    int wt[n] = {2, 3, 5, 10};
+    int DP[n + 1][w + 1];
+
+    for (i = 0; i < n + 1; i++)
+    {
+        for (j = 0; j < w + 1; j++)
+        {
+            if (i == 0)
+                DP[i][j] = 0;
+            else if (j == 0)
+                DP[i][j] = 1;
+            else if (j < wt[i - 1])
+                DP[i][j] = DP[i - 1][j];
+            else
+                DP[i][j] = DP[i][j - wt[i - 1]] + DP[i - 1][j];
+            cout << DP[i][j] << " ";
+        }
+        cout << " \n";
+    }
+    cout << "Total number of ways: " << DP[n][w];
+}
