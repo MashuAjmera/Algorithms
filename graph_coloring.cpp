e7332695c2a6108a077f63c0f93e9594ff7b1158
Mashu Ajmera <mashuajmera@gmail.com>
2021-03-24 08:15:21 +0530

    identify whether a graph can be colored using the given number of colors

---
+++ graph_coloring.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+#define N 4
+
+bool isSafe(int graph[][N], int colors[], int level)
+{
+    for (int i = 0; i < level; i++)
+    {
+        if (graph[level][i] == 1 && colors[level] == colors[i])
+            return false;
+    }
+    return true;
+}
+
+bool colorGraph(int graph[][N], int m, int colors[N], int level)
+{
+    if (level >= N)
+    {
+        cout << "\nThe colors are:";
+        for (int i = 0; i < N; i++)
+        {
+            cout << " " << colors[i];
+        }
+        return true;
+    }
+
+    bool res = false;
+    for (int i = 0; i < m; i++)
+    {
+        colors[level] = i;
+        if (isSafe(graph, colors, level))
+        {
+            res = colorGraph(graph, m, colors, level + 1) || res;
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    int graph[][N] = {{0, 1, 0, 1},
+                      {1, 0, 1, 0},
+                      {0, 1, 0, 1},
+                      {1, 0, 1, 0}};
+    int colors[N];
+    int m = 3;
+    if (!colorGraph(graph, m, colors, 0))
+    {
+        cout << "graph cannot be colored using " << m << " colors";
+    };
+}
