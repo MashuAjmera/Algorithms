c413913799c0d0b8d56f8d578bc956551dbfda02
Mashu Ajmera <mashuajmera@gmail.com>
2021-03-24 08:16:04 +0530

    find ht emaximum subarray sum for a given array using Kadane's algorithm

---
+++ kadane.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    // int arr[] = {5, -4, -2, 6, -1};
+    int arr[] = {-5, 4, 6, -3, 4, -1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int max = INT_MIN, current = 0;
+    for (int i = 0; i < n; i++)
+    {
+        current += arr[i];
+        if (max < current)
+            max = current;
+        if (current < 0)
+            current = 0;
+    }
+    cout << "The maximum subarray sum is: " << max;
+}
